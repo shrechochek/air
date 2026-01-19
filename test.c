5ebe5202fc0779ae233d2ce4f000c773be2e45be
shrechochek <stephan.martynov41@gmail.com>
2026-01-19 20:54:09 +0300

    add string data type

--- test.c
+++ test.c
@@ -26,9 +26,13 @@ char* skip_whitespace(char *str) {
 #define MAX_VARS 100
 #define VAR_NAME_LEN 63
 
+typedef enum { VAR_INT, VAR_STRING } VarType; //var types
+
 typedef struct {
     char name[VAR_NAME_LEN + 1];
-    int value;
+    VarType type;
+    int ivalue;
+    char svalue[256];
     bool in_use;
 } Variable;
 
@@ -51,28 +55,50 @@ int find_var(const char *name) {
     return -1;
 }
 
-bool set_var(const char *name, int value) {
+bool set_var_int(const char *name, int value) {
     int idx = find_var(name);
-    if (idx >= 0) { //if found var we set new value
-        vars[idx].value = value;
+    if (idx >= 0) {
+        vars[idx].type = VAR_INT;
+        vars[idx].ivalue = value;
         return true;
     }
-    for (int i = 0; i < MAX_VARS; i++) { //if we didn't find var we create var
+    for (int i = 0; i < MAX_VARS; i++) {
         if (!vars[i].in_use) {
             vars[i].in_use = true;
             strncpy(vars[i].name, name, VAR_NAME_LEN);
             vars[i].name[VAR_NAME_LEN] = '\0';
-            vars[i].value = value;
+            vars[i].type = VAR_INT;
+            vars[i].ivalue = value;
             return true;
         }
     }
     return false;
 }
 
+bool set_var_string(const char *name, const char *value) {
+    int idx = find_var(name);
+    if (idx < 0) {
+        for (int i = 0; i < MAX_VARS; i++) {
+            if (!vars[i].in_use) {
+                idx = i;
+                vars[i].in_use = true;
+                strncpy(vars[i].name, name, VAR_NAME_LEN);
+                vars[i].name[VAR_NAME_LEN] = '\0';
+                break;
+            }
+        }
+        if (idx < 0) return false;
+    }
+    vars[idx].type = VAR_STRING;
+    strncpy(vars[idx].svalue, value, sizeof(vars[idx].svalue) - 1);
+    vars[idx].svalue[sizeof(vars[idx].svalue) - 1] = '\0';
+    return true;
+}
+
 bool get_var(const char *name, int *out) {
     int idx = find_var(name);
     if (idx < 0) return false;
-    *out = vars[idx].value;
+    *out = vars[idx].ivalue;
     return true;
 }
 
@@ -119,9 +145,13 @@ int main(void) {
                 if (end_quote) *end_quote = '\0';
                 printf("%s\n", print_token + 1);
             } else { //if var
-                int value;
-                if (get_var(print_token, &value)) {
-                    printf("%d\n", value);
+                int idx = find_var(print_token);
+                if (idx >= 0) {
+                    if (vars[idx].type == VAR_INT) {
+                        printf("%d\n", vars[idx].ivalue);
+                    } else if (vars[idx].type == VAR_STRING) {
+                        printf("%s\n", vars[idx].svalue);
+                    }
                 } else {
                     fprintf(stderr, "Unknown variable: %s\n", print_token);
                 }
@@ -137,10 +167,27 @@ int main(void) {
             remove_end(name_start);
             char *value_start = skip_whitespace(eq + 1);
             int value = atoi(value_start);
-            if (!set_var(name_start, value)) {
+            if (!set_var_int(name_start, value)) {
                 fprintf(stderr, "Variable storage full\n");
             }
-        } else {
+        } else if (strncmp(token, "string", 3) == 0 && isspace((unsigned char)token[6])) { // if string var
+            // string myStr = "hello";
+            char *name_start = skip_whitespace(token + 6);
+            char *eq = strchr(name_start, '=');
+            *eq = '\0';
+            remove_end(name_start);
+            char *value_start = skip_whitespace(eq + 1);
+
+            // обрезаем кавычки
+            if (*value_start == '"') {
+                char *end_quote = strrchr(value_start + 1, '"');
+                if (end_quote) *end_quote = '\0';
+                value_start++; // теперь указывает на первый символ внутри кавычек
+            }
+
+            set_var_string(name_start, value_start);
+        } 
+        else {
             // add new event?
         }
         token = strtok(NULL, ";");
